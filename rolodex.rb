0b01710bf57aceedeec716468b0aeff89b7149fe
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-14 16:59:55 -0400

    Rolodex to be reviewed

--- rolodex.rb
+++ rolodex.rb
@@ -1,18 +1,22 @@
+require_relative "contact.rb"
+
 class Rolodex
-	
+		@@id = 1000
+
 	def initialize
-	@id = 1000
-	@contacts = []
-end
-
-	def contacts
-		@contacts = contact
+		@contacts = []
 	end
 
-		def add_contact(contact)
-			contact.id = @id
-			@contacts << contacts
-			@@id += 1
-		end
+
+	def add_contact(first_name, last_name, email, note)
+		contact = Contact.new(@@id, @first_name, @last_name, @email, @notes)
+		@@id += 1		
+		
+		@contacts << contacts
 	end
+
+	def all
+		@contacts
+	end
+
 end
