9cef18b99e2c9aa764d2a1ae376b2d271b1ac363
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-14 15:57:16 -0400

    Contact is complete

---
+++ contact.rb
@@ -0,0 +1,19 @@
+
+class Contact
+attr_accessor :first_name; :last_name; :email; :notes
+attr_reader :id
+
+	def initialize(id, first_name, last_name, email, notes)
+	@id = id
+	@first_name = first_name.capitalize
+	@last_name = last_name.capitalize
+	@email = email.downcase
+	@notes = notes
+	end
+
+		def full_name
+		"#{first_name} #{last_name}" 
+		end
+
+end
+
